5794e5922a2f005dfc557952599ad6decfc8c97f
Hai <hou.nghoanghai@gmail.com>
2021-01-23 12:02:19 +0700

    23/01/2021

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 (function uploadFileClickHandle() {
     var uploadFileBtn = document.querySelector("#upload-file-btn");
-
     var uploadFileInput = document.querySelector("#upload-file-input");
+    var fileName = document.querySelector("#file-name");
+    var cardPDF = document.querySelector("#card-pdf");
+    var dropArea = document.querySelector("#drop-area");
+    var fileSize = document.querySelector("#file-size");
+
+    function formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
+
+    function setText(v){
+        fileSize.innerHTML = formatBytes(v);
+    }
 
     uploadFileBtn.addEventListener("click", function () {
         uploadFileInput.click();
+    });
+
+    uploadFileInput.addEventListener("change", function () {
+        dropArea.style.display = "none";
+        cardPDF.style.display = "block";
+        fileName.textContent = this.files[0].name;
+        setText(this.files[0].size);
     });
 })();
 (function dragAndDropUploadHandle() {
@@ -21,11 +47,11 @@
         console.log(e, this);
 
         if (!e.dataTransfer || e.dataTransfer.files.length < 1) return;
-        
+
         // var files = e.dataTransfer.files;
 
         // files.forEach(function(file) {
-            
+
         // })
     }
 })();
--- js/pages/create-signature-request.js
+++ js/pages/create-signature-request.js
@@ -1,31 +1,40 @@
 (function () {
     function initRecipientForm() {
-        var originalRecipientBlock = document.querySelector('#original-recipient-block');
-        var recipientList = document.querySelector('#recipient-list');
-        var addRecipientButton = document.querySelector('#add-recipient-button');
-        var addOrderRecipient = document.querySelector('#ordered-checkbox');
-        var orderInput = document.querySelector('#order-input');
-        // var deleteRecipentButton = document.querySelector('#delete-recipient-button');
+        var newRecipientBlock = document.querySelector("#new-recipient-block");
+        var recipientList = document.querySelector("#recipient-list");
+        var addRecipientButton = document.querySelector("#add-recipient-button");
+        var addOrderRecipient = document.querySelector("#ordered-checkbox");
+        var orderInput = document.querySelector("#order-input");
+        var deleteButton = document.querySelector('#delete-btn')
 
-        originalRecipientBlock.removeAttribute('id');
+        newRecipientBlock.removeAttribute("id");
 
-        addRecipientButton.addEventListener('click', function () {
-            var cloned = originalRecipientBlock.cloneNode(true);
+        addRecipientButton.addEventListener("click", function () {
+            newRecipientBlock.style.display = ('flex');
+            var cloned = newRecipientBlock.cloneNode(true);
             recipientList.append(cloned);
+            deleteButton.classList.remove("w-hidden");
         });
 
-        addOrderRecipient.addEventListener('click', function () {
-            if (addOrderRecipient.checked == true) {
-                orderInput.style.display = 'block';
-            } else {
-                orderInput.style.display = 'none';
-            }
-        })
+        addOrderRecipient.addEventListener("click", function (event) {
+            event.stopPropagation();
 
-        // deleteRecipentButton.addEventListener('click', function() {
-        //     recipientList.removeChild(originalRecipientBlock);
-        // })
+            if (addOrderRecipient.checked == true) {
+                orderInput.style.display = "block";
+            } else {
+                orderInput.style.display = "none";
+            }
+        });
+
+        recipientList.addEventListener("click", function (event) {
+            event.stopPropagation();
+
+            if (!event.target.classList.contains("delete-recipient-button"))
+                return;
+
+            event.target.closest(".recipient").remove();
+        });
     }
 
     initRecipientForm();
-})()
+})();
